be4fcdaa68647f01cdeb4f0d7cdc480a36df464e
princess maggy <edoziemagdalene@gmail.com>
2024-03-26 13:21:07 +0100

    search component

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar, StyleSheet, Text, View , SafeAreaView} from 'react-native';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { useState } from "react";
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
-    <SafeAreaView style={{flex:1, marginTop:StatusBar.currentHeight}}>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.cont}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.search}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={setSearchQuery}
+            value={searchQuery}
+          />
+        </View>
+        <View style={styles.list}>
+        <Text>Open up App.js to start working on your app!</Text></View>
       </View>
-      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  cont: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+    // marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+    width: 300,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    
   },
 });
