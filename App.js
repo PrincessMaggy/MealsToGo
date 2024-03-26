ce4790015e7a03d660ce4ff53fb5272b0d0ed3f3
princess maggy <edoziemagdalene@gmail.com>
2024-03-26 12:25:05 +0100

    eslint & prettier

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View , SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
+    <SafeAreaView style={{flex:1, marginTop:StatusBar.currentHeight}}>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
