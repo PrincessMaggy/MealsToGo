3376a8a20437ab79a0dc5b6feffd78ee2dd464bc
princess maggy <edoziemagdalene@gmail.com>
2024-03-30 10:05:01 +0100

    setting up font

--- src/features/restaurants/components/restaurantInfoCard.component.js
+++ src/features/restaurants/components/restaurantInfoCard.component.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
+import { Text } from "react-native";
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.colors.ui.primary};
+const Title = styled(Text)`
+  color: ${(props) => props.theme.colors.ui.error};
   padding: ${(props) => props.theme.space[1]};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.h1};
 `;
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -16,7 +20,7 @@ const RestaurantCardCover = styled(Card.Cover)`
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const {
-    name = "Wizzy maestro",
+    name = "Wizzy Maestro",
     icon,
     photos = [
       "https://img.freepik.com/free-photo/top-view-fast-food-mix-mozzarella-sticks-club-sandwich-hamburger-mushroom-pizza-caesar-shrimp-salad-french-fries-ketchup-mayo-cheese-sauces-table_141793-3998.jpg?w=1480&t=st=1711461107~exp=1711461707~hmac=ac12768ce37a5419370d5a7c5a4a19a4a8833d01af0ea63a9e3677345fd6549e",
