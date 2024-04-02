025f09b2c62daede80aa1dc44da281d3f0a71de5
princess maggy <edoziemagdalene@gmail.com>
2024-04-02 16:03:12 +0100

    setting map region

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styled from "styled-components/native";
 import MapView from "react-native-maps";
 import { SafeArea } from "../../../components/utility/safearea.component";
 import { SearchMap } from "../components/mapsearch.component";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 const Map = styled(MapView)`
   height: 100%;
@@ -10,11 +12,33 @@ const Map = styled(MapView)`
 `;
 
 export const MapScreen = ({ navigation }) => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+  const [latDelta, setLatDelta] = useState(0);
+  const { viewport, lat, lng } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+    const latDelta = northeastLat - southwestLat;
+    setLatDelta(latDelta);
+  }, [location, viewport]);
+
   return (
     <SafeArea>
-      <SearchMap/>
-
-      <Map/>
+      <SearchMap />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((item) => {
+          return null;
+        })}
+      </Map>
     </SafeArea>
   );
 };
