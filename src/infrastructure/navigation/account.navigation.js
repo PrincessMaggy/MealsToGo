d239de3a8123c7d74d12aebeda198b6f096c9d20
princess maggy <edoziemagdalene@gmail.com>
2024-04-04 12:14:46 +0100

    login screen

--- src/infrastructure/navigation/account.navigation.js
+++ src/infrastructure/navigation/account.navigation.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { AccountScreen } from "../../features/account/screens/accounts.screen";
+import { LoginScreen } from "../../features/account/screens/login.screen";
 
 const Stack = createStackNavigator();
 
 export const AccountNavigator = () => (
   <Stack.Navigator options={{ headerShown: false }}>
     <Stack.Screen name="Main" component={AccountScreen} />
-    <Stack.Screen name="Login" component={AccountScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Register" component={AccountScreen} />
   </Stack.Navigator>
 );
